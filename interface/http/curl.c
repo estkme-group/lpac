4fbda48123543df4a95ede9ca464610abd0e7b20
estkme <145633413+estkme@users.noreply.github.com>
2023-11-14 18:03:22 +0800

    new curl interface

--- interface/http/curl.c
+++ interface/http/curl.c
@@ -1,10 +1,50 @@
 #include <inttypes.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <curl/curl.h>
+#ifdef __MINGW32__
+#include <dlfcn-win32/dlfcn.h>
+#else
+#include <dlfcn.h>
+#endif
 
 #include <euicc/interface.h>
+
+/* BEGIN MINIMAL CURL DEFINE */
+#define CURL_GLOBAL_DEFAULT ((1 << 0) | (1 << 1))
+#define CURLE_OK 0
+#define CURLOPT_URL 10002
+#define CURLOPT_WRITEFUNCTION 20011
+#define CURLOPT_WRITEDATA 10001
+#define CURLOPT_SSL_VERIFYPEER 64
+#define CURLOPT_SSL_VERIFYHOST 81
+#define CURLOPT_HTTPHEADER 10023
+#define CURLOPT_POSTFIELDS 10015
+#define CURLOPT_POSTFIELDSIZE 60
+#define CURLINFO_RESPONSE_CODE 2097154
+
+typedef void CURL;
+typedef int CURLcode;
+typedef int CURLoption;
+typedef int CURLINFO;
+
+static void *libcurl_interface_dlhandle = NULL;
+
+static struct libcurl_interface
+{
+    CURLcode (*curl_global_init)(long flags);
+    CURL *(*curl_easy_init)(void);
+    CURLcode (*curl_easy_setopt)(CURL *curl, CURLoption option, ...);
+    CURLcode (*curl_easy_perform)(CURL *curl);
+    CURLcode (*curl_easy_getinfo)(CURL *curl, CURLINFO info, ...);
+    const char (*curl_easy_strerror)(CURLcode);
+    void (*curl_easy_cleanup)(CURL *curl);
+
+    struct curl_slist *(*curl_slist_append)(struct curl_slist *list, const char *data);
+    void (*curl_slist_free_all)(struct curl_slist *list);
+} libcurl;
+/* END MINIMAL CURL DEFINE */
 
 struct http_trans_response_data
 {
@@ -50,43 +90,43 @@ static int http_interface_transmit(const char *url, uint32_t *rcode, uint8_t **r
     (*rx) = NULL;
     (*rcode) = 0;
 
-    curl = curl_easy_init();
+    curl = libcurl.curl_easy_init();
     if (!curl)
     {
         goto err;
     }
 
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, http_trans_write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&responseData);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    libcurl.curl_easy_setopt(curl, CURLOPT_URL, url);
+    libcurl.curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, http_trans_write_callback);
+    libcurl.curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&responseData);
+    libcurl.curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    libcurl.curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
     for (int i = 0; h[i] != NULL; i++)
     {
-        nheaders = curl_slist_append(headers, h[i]);
+        nheaders = libcurl.curl_slist_append(headers, h[i]);
         if (nheaders == NULL)
         {
             goto err;
         }
         headers = nheaders;
     }
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    libcurl.curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
     if (tx != NULL)
     {
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, tx);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, tx_len);
+        libcurl.curl_easy_setopt(curl, CURLOPT_POSTFIELDS, tx);
+        libcurl.curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, tx_len);
     }
 
-    res = curl_easy_perform(curl);
+    res = libcurl.curl_easy_perform(curl);
 
     if (res != CURLE_OK)
     {
-        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", libcurl.curl_easy_strerror(res));
         goto err;
     }
 
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+    libcurl.curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
     *rcode = response_code;
     *rx = responseData.data;
     *rx_len = responseData.size;
@@ -98,16 +138,45 @@ err:
     fret = -1;
     free(responseData.data);
 exit:
-    curl_easy_cleanup(curl);
-    curl_slist_free_all(headers);
+    libcurl.curl_easy_cleanup(curl);
+    libcurl.curl_slist_free_all(headers);
     return fret;
 }
 
 int libhttpinterface_main(struct euicc_http_interface *ifstruct)
 {
+    const char *libcurl_path;
+
     memset(ifstruct, 0, sizeof(struct euicc_http_interface));
 
-    if (curl_global_init(CURL_GLOBAL_DEFAULT) != CURLE_OK)
+    if (!(libcurl_path = getenv("LIBCURL")))
+    {
+#if defined(__MINGW32__)
+libcurl_path = "./libcurl.dll";
+#elif defined(__APPLE__)
+libcurl_path = "./libcurl.dylib";
+#else
+libcurl_path = "libcurl.so.4";
+#endif
+    }
+
+    if (!(libcurl_interface_dlhandle = dlopen(libcurl_path, RTLD_LAZY)))
+    {
+        fprintf(stderr, "libcurl env missing, current: LIBCURL=%s err:%s\n", libcurl_path, dlerror());
+        return -1;
+    }
+
+    libcurl.curl_global_init = dlsym(libcurl_interface_dlhandle, "curl_global_init");
+    libcurl.curl_easy_init = dlsym(libcurl_interface_dlhandle, "curl_easy_init");
+    libcurl.curl_easy_setopt = dlsym(libcurl_interface_dlhandle, "curl_easy_setopt");
+    libcurl.curl_easy_perform = dlsym(libcurl_interface_dlhandle, "curl_easy_perform");
+    libcurl.curl_easy_getinfo = dlsym(libcurl_interface_dlhandle, "curl_easy_getinfo");
+    libcurl.curl_easy_strerror = dlsym(libcurl_interface_dlhandle, "curl_easy_strerror");
+    libcurl.curl_easy_cleanup = dlsym(libcurl_interface_dlhandle, "curl_easy_cleanup");
+    libcurl.curl_slist_append = dlsym(libcurl_interface_dlhandle, "curl_slist_append");
+    libcurl.curl_slist_free_all = dlsym(libcurl_interface_dlhandle, "curl_slist_free_all");
+
+    if (libcurl.curl_global_init(CURL_GLOBAL_DEFAULT) != CURLE_OK)
     {
         return -1;
     }
