9dcd285e6e016bebb447189cf5af5276d5cd440d
septs <github@septs.pw>
2024-03-16 16:52:32 +0800

    accpet review

--- src/applet/version.c
+++ src/applet/version.c
@@ -2,6 +2,10 @@
 
 #include <main.h>
 
+#ifndef LPAC_VERSION
+#define LPAC_VERSION "v0.0.0-unknown"
+#endif
+
 static int applet_main(int argc, char **argv)
 {
     jprint_success(cJSON_CreateString(LPAC_VERSION));
