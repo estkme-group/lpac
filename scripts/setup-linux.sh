77219eadcbeb465bcb783b898e1f03a293fe22fe
septs <github@septs.pw>
2024-03-16 14:11:36 +0800

    improve build.sh

--- scripts/setup-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-sudo apt-get update
-sudo apt-get install -y libpcsclite-dev libcurl4-openssl-dev gcc make cmake gcc-mingw-w64 g++-mingw-w64 unzip ninja-build
-sudo snap install zig --classic --beta
