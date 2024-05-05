f043b5cf9702dc7dfa452d426d4f0e5866643293
septs <github@septs.pw>
2024-05-05 22:22:15 +0800

    chore: improve is_valid_fqdn_name

--- src/utils.c
+++ src/utils.c
@@ -9,19 +9,24 @@ bool is_valid_fqdn_name(const char *name)
     {
         if (name[i] == '.')
         {
+            if (count == 0)
+            {
+                return false;
+            }
             count = 0;
             continue;
         }
         allowed = (name[i] >= '0' && name[i] <= '9') ||
                   (name[i] >= 'a' && name[i] <= 'z') ||
-                  (name[i] >= 'A' && name[i] <= 'Z');
+                  (name[i] >= 'A' && name[i] <= 'Z') ||
+                  name[i] == '_' || name[i] == '-';
         if (!allowed || count > 63)
         {
             return false;
         }
         count++;
     }
-    if (count > 63)
+    if (count > 63 || count == 0)
     {
         return false;
     }
