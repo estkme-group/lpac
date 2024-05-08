d145cfcb8dba02dbd87d6c627246104e9f229019
septs <github@septs.pw>
2024-05-08 09:53:24 +0800

    feat: add detail to progress event

--- src/jprint.h
+++ src/jprint.h
@@ -2,5 +2,5 @@
 #include <cjson/cJSON_ex.h>
 
 void jprint_error(const char *function_name, const char *detail);
-void jprint_progress(const char *function_name);
+void jprint_progress(const char *function_name, const char *detail);
 void jprint_success(cJSON *jdata);
