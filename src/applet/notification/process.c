db5dc14fb61081b0c060db63748d3b08193e935a
septs <github@septs.pw>
2024-05-09 21:51:10 +0800

    feat: batch handling notification {process,remove} (#82)

--- src/applet/notification/process.c
+++ src/applet/notification/process.c
@@ -1,78 +1,91 @@
 #include "process.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 #include <main.h>
 
 #include <euicc/es10b.h>
 #include <euicc/es9p.h>
 
-static void _help(const char *applet_name)
-{
-    printf("Usage: %s [seqNumber] [-r]\r\n", applet_name);
-    printf("\t -r\tAutomatically remove processed notifications\r\n");
-}
+static const char *opt_string = "rh?";
 
 static int applet_main(int argc, char **argv)
 {
-    unsigned long seqNumber;
-    char *str_seqNumber = NULL;
-    int autoremove = 0;
-    struct es10b_pending_notification notification;
-
-    for (int i = 0; i < argc; i++)
+    bool auto_remove = false;
+    int opt = getopt(argc, argv, opt_string);
+    while (opt != -1)
     {
-        if (strcmp(argv[i], "-h") == 0)
+        switch (opt)
         {
-            _help(argv[0]);
+            case 'r':
+                auto_remove = true;
+                break;
+            case 'h':
+            case '?':
+                printf("Usage: %s [seqNumber] etc [OPTIONS]\r\n", argv[0]);
+                printf("\t -r Automatically remove processed notifications\r\n");
+                return -1;
+            default:
+                break;
+        }
+        opt = getopt(argc, argv, opt_string);
+    }
+
+    int seqNumber;
+    char *str_seqNumber = NULL;
+    struct es10b_pending_notification notification;
+    int ret;
+
+    for (int i = 1; i < argc; i++)
+    {
+        errno = 0;
+        seqNumber = (int) strtol(argv[i], NULL, 10);
+        if (errno != 0)
+        {
+            continue;
+        }
+        str_seqNumber = argv[i];
+
+        jprint_progress("es10b_retrieve_notifications_list", str_seqNumber);
+        if (es10b_retrieve_notifications_list(&euicc_ctx, &notification, seqNumber))
+        {
+            jprint_error("es10b_retrieve_notifications_list", NULL);
             return -1;
         }
-        else if (strcmp(argv[i], "-r") == 0)
+
+        euicc_ctx.http.server_address = notification.notificationAddress;
+
+        jprint_progress("es9p_handle_notification", str_seqNumber);
+        if (es9p_handle_notification(&euicc_ctx, notification.b64_PendingNotification))
         {
-            autoremove = 1;
+            jprint_error("es9p_handle_notification", NULL);
+            return -1;
         }
-        else
+
+        es10b_pending_notification_free(&notification);
+
+        if (!auto_remove)
         {
-            if (i && !str_seqNumber)
-            {
-                str_seqNumber = argv[i];
-            }
+            continue;
         }
-    }
 
-    if (str_seqNumber == NULL)
-    {
-        _help(argv[0]);
-        return -1;
-    }
-
-    seqNumber = atol(str_seqNumber);
-
-    jprint_progress("es10b_retrieve_notifications_list", str_seqNumber);
-    if (es10b_retrieve_notifications_list(&euicc_ctx, &notification, seqNumber))
-    {
-        jprint_error("es10b_retrieve_notifications_list", NULL);
-        return -1;
-    }
-
-    euicc_ctx.http.server_address = notification.notificationAddress;
-
-    jprint_progress("es9p_handle_notification", str_seqNumber);
-    if (es9p_handle_notification(&euicc_ctx, notification.b64_PendingNotification))
-    {
-        jprint_error("es9p_handle_notification", NULL);
-        return -1;
-    }
-
-    es10b_pending_notification_free(&notification);
-
-    if (autoremove)
-    {
         jprint_progress("es10b_remove_notification_from_list", str_seqNumber);
-        if (es10b_remove_notification_from_list(&euicc_ctx, seqNumber))
+        if ((ret = es10b_remove_notification_from_list(&euicc_ctx, seqNumber)))
         {
-            jprint_error("es10b_remove_notification_from_list", NULL);
+            const char *reason;
+            switch (ret)
+            {
+                case 1:
+                    reason = "seqNumber not found";
+                    break;
+                default:
+                    reason = "unknown";
+                    break;
+            }
+            jprint_error("es10b_remove_notification_from_list", reason);
             return -1;
         }
     }
--- src/applet/notification/remove.c
+++ src/applet/notification/remove.c
@@ -2,38 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <string.h>
 #include <main.h>
 
 #include <euicc/es10b.h>
 
+static const char *opt_string = "h?";
+
 static int applet_main(int argc, char **argv)
 {
-    int ret;
-    unsigned long seqNumber;
-
-    if (argc < 2)
+    int opt = getopt(argc, argv, opt_string);
+    while (opt != -1)
     {
-        printf("Usage: %s <seqNumber>\n", argv[0]);
-        return -1;
+        switch (opt)
+        {
+            case 'h':
+            case '?':
+                printf("Usage: %s [seqNumber] etc\r\n", argv[0]);
+                return -1;
+            default:
+                break;
+        }
+        opt = getopt(argc, argv, opt_string);
     }
 
-    seqNumber = atol(argv[1]);
+    int seqNumber;
+    int ret;
 
-    if ((ret = es10b_remove_notification_from_list(&euicc_ctx, seqNumber)))
+    for (int i = 1; i < argc; i++)
     {
-        const char *reason;
-        switch (ret)
+        errno = 0;
+        seqNumber = (int) strtol(argv[i], NULL, 10);
+        if (errno != 0)
         {
-        case 1:
-            reason = "seqNumber not found";
-            break;
-        default:
-            reason = "unknown";
-            break;
+            continue;
         }
-        jprint_error("es10b_remove_notification_from_list", reason);
-        return -1;
+        if ((ret = es10b_remove_notification_from_list(&euicc_ctx, seqNumber)))
+        {
+            const char *reason;
+            switch (ret)
+            {
+                case 1:
+                    reason = "seqNumber not found";
+                    break;
+                default:
+                    reason = "unknown";
+                    break;
+            }
+            jprint_error("es10b_remove_notification_from_list", reason);
+            return -1;
+        }
     }
 
     jprint_success(NULL);
