4fb974e321b38f2c835aecb5cd51663ac82ae6e3
Andy Schroder <info@andyschroder.com>
2024-05-29 22:27:25 -0400

    add zip dependency in setup-debian.sh for build.sh

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xeuo pipefail
 sudo apt-get update
-sudo apt-get install -y build-essential libpcsclite-dev libcurl4-openssl-dev gcc-mingw-w64 g++-mingw-w64
+sudo apt-get install -y build-essential libpcsclite-dev libcurl4-openssl-dev gcc-mingw-w64 g++-mingw-w64 zip
