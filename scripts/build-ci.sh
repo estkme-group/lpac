2c63dfdef6e8b873af7022e0cbcf147d23a26dcf
septs <github@septs.pw>
2024-06-21 11:17:31 +0800

    refactor: scripts

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -1,95 +1,52 @@
 #!/bin/bash
-set -euo pipefail
-WORKSPACE="$(pwd)"
-KERNEL="$(uname -s)"
-MATCHINE="$(uname -m)"
-CURL_VERSION="8.6.0_1"
-WOA_TOOLCHAIN_VERSION="2024-02-08"
-MINGW_CURL_WIN64_BLOB="https://curl.se/windows/dl-$CURL_VERSION/curl-$CURL_VERSION-win64-mingw.zip"
-MINGW_CURL_WIN64A_BLOB="https://curl.se/windows/dl-$CURL_VERSION/curl-$CURL_VERSION-win64a-mingw.zip"
-MINGW32_TOOLCHAIN_BLOB="https://github.com/Windows-on-ARM-Experiments/mingw-woarm64-build/releases/download/$WOA_TOOLCHAIN_VERSION/aarch64-w64-mingw32-msvcrt-toolchain.tar.gz"
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-case "$KERNEL" in
-Linux)
-    KERNEL="linux"
-    ;;
-Darwin)
-    KERNEL="darwin"
-    MATCHINE="universal"
-    ;;
-esac
+source "$SCRIPT_DIR/functions.sh"
 
-function download {
-    URL="$1"
-    SAVED_PATH="$(mktemp)"
-    SAVED_DIR="$(mktemp -d)"
-    wget --no-verbose "$URL" -O "$SAVED_PATH"
-    case "$URL" in
-    *.zip)
-        unzip -q -d "$SAVED_DIR" "$SAVED_PATH"
-        rm "$SAVED_PATH"
-        echo "$SAVED_DIR"
-        ;;
-    *.tar.gz)
-        tar -C "$SAVED_DIR" --gzip --extract --file="$SAVED_PATH"
-        rm "$SAVED_PATH"
-        echo "$SAVED_DIR"
-        ;;
-    *)
-        echo "$SAVED_PATH"
-        ;;
-    esac
-}
-
-set -x
+set -xeuo pipefail
 
 BUILD="$(mktemp -d)"
-cd "$BUILD" || exit 1
-
 ARTIFACT="$WORKSPACE/build"
 
-mkdir "$BUILD/output"
-[ -d "$ARTIFACT" ] || mkdir "$ARTIFACT"
+cd "$BUILD" || exit 1
 
-cp "$WORKSPACE/src/LICENSE" "$BUILD/output/lpac-LICENSE"
-cp "$WORKSPACE/euicc/LICENSE" "$BUILD/output/libeuicc-LICENSE"
-cp "$WORKSPACE/cjson/LICENSE" "$BUILD/output/cjson-LICENSE"
-cp "$WORKSPACE/dlfcn-win32/LICENSE" "$BUILD/output/dlfcn-win32-LICENSE"
+mkdir -p "$BUILD/output"
+mkdir -p "$ARTIFACT"
 
 case "${1:-}" in
 make)
     cmake "$WORKSPACE"
     make -j
+    copy-license "$BUILD/output"
     zip -r -j "$ARTIFACT/lpac-$KERNEL-$MATCHINE.zip" output/*
     ;;
 debian)
     cmake "$WORKSPACE" -DCPACK_GENERATOR=DEB
     make -j package
+    copy-license "$BUILD/output"
     cp lpac_*.deb "$ARTIFACT"
     ;;
 mingw)
     cmake "$WORKSPACE" -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw64.cmake
     make -j
-    CURL="$(download "$MINGW_CURL_WIN64_BLOB")"
-    cp "$CURL"/curl-*-mingw/bin/libcurl-x64.dll output/libcurl.dll
-    cp "$CURL"/curl-*-mingw/COPYING.txt output/libcurl-LICENSE
+    copy-license "$BUILD/output"
+    copy-curl-win "$BUILD/output"
     zip -r -j "$ARTIFACT/lpac-windows-x86_64-mingw.zip" output/*
     ;;
 woa-mingw)
     TOOLCHAIN="$(download "$MINGW32_TOOLCHAIN_BLOB")"
     cmake "$WORKSPACE" -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw64-woa.cmake "-DTOOLCHAIN_BIN_PATH=$TOOLCHAIN/bin"
     make -j
-    CURL="$(download "$MINGW_CURL_WIN64A_BLOB")"
-    cp "$CURL"/curl-*-mingw/bin/libcurl-arm64.dll output/libcurl.dll
-    cp "$CURL"/curl-*-mingw/COPYING.txt output/libcurl-LICENSE
+    rm -rf "$TOOLCHAIN"
+    copy-license "$BUILD/output"
+    copy-curl-woa "$BUILD/output"
     zip -r -j "$ARTIFACT/lpac-windows-arm64-mingw.zip" output/*
     ;;
 woa-zig)
     cmake "$WORKSPACE" -DCMAKE_TOOLCHAIN_FILE=./cmake/aarch64-windows-zig.cmake
     make -j
-    CURL="$(download "$MINGW_CURL_WIN64A_BLOB")"
-    cp "$CURL"/curl-*-mingw/bin/libcurl-arm64.dll output/libcurl.dll
-    cp "$CURL"/curl-*-mingw/COPYING.txt output/libcurl-LICENSE
+    copy-license "$BUILD/output"
+    copy-curl-woa "$BUILD/output"
     zip -r -j "$ARTIFACT/lpac-windows-arm64-zig.zip" output/*
     ;;
 *)
---
+++ scripts/functions.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+export KERNEL="$(uname -s)"
+export MATCHINE="$(uname -m)"
+
+export WORKSPACE="${GITHUB_WORKSPACE:-$(pwd)}"
+export CURL_VERSION="8.6.0_1"
+export WOA_TOOLCHAIN_VERSION="2024-02-08"
+export MINGW_CURL_WIN64_BLOB="https://curl.se/windows/dl-$CURL_VERSION/curl-$CURL_VERSION-win64-mingw.zip"
+export MINGW_CURL_WIN64A_BLOB="https://curl.se/windows/dl-$CURL_VERSION/curl-$CURL_VERSION-win64a-mingw.zip"
+export MINGW32_TOOLCHAIN_BLOB="https://github.com/Windows-on-ARM-Experiments/mingw-woarm64-build/releases/download/$WOA_TOOLCHAIN_VERSION/aarch64-w64-mingw32-msvcrt-toolchain.tar.gz"
+
+case "$KERNEL" in
+Linux)
+    KERNEL="linux"
+    ;;
+Darwin)
+    KERNEL="darwin"
+    MATCHINE="universal"
+    ;;
+esac
+
+function download {
+    local URL="$1"
+    local SAVED_PATH="$(mktemp)"
+    local SAVED_DIR="$(mktemp -d)"
+    wget --no-verbose "$URL" -O "$SAVED_PATH"
+    case "$URL" in
+    *.zip)
+        unzip -q -d "$SAVED_DIR" "$SAVED_PATH"
+        rm "$SAVED_PATH"
+        echo "$SAVED_DIR"
+        ;;
+    *.tar.gz)
+        tar -C "$SAVED_DIR" --gzip --extract --file="$SAVED_PATH"
+        rm "$SAVED_PATH"
+        echo "$SAVED_DIR"
+        ;;
+    *)
+        echo "$SAVED_PATH"
+        ;;
+    esac
+}
+
+function copy-license {
+    local OUTPUT="$1"
+
+    cp "$WORKSPACE/src/LICENSE" "$OUTPUT/lpac-LICENSE"
+    cp "$WORKSPACE/euicc/LICENSE" "$OUTPUT/libeuicc-LICENSE"
+    cp "$WORKSPACE/cjson/LICENSE" "$OUTPUT/cjson-LICENSE"
+    cp "$WORKSPACE/dlfcn-win32/LICENSE" "$OUTPUT/dlfcn-win32-LICENSE"
+}
+
+function copy-curl-woa {
+    local OUTPUT="$1"
+
+    CURL="$(download "$MINGW_CURL_WIN64A_BLOB")"
+    cp "$CURL"/curl-*-mingw/bin/libcurl-arm64.dll "$OUTPUT/libcurl.dll"
+    cp "$CURL"/curl-*-mingw/COPYING.txt "$OUTPUT/libcurl-LICENSE"
+    rm -rf "$CURL"
+}
+
+function copy-curl-win {
+    local OUTPUT="$1"
+
+    CURL="$(download "$MINGW_CURL_WIN64_BLOB")"
+    cp "$CURL"/curl-*-mingw/bin/libcurl-x64.dll "$OUTPUT/libcurl.dll"
+    cp "$CURL"/curl-*-mingw/COPYING.txt "$OUTPUT/libcurl-LICENSE"
+    rm -rf "$CURL"
+}
