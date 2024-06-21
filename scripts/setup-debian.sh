c75726b38dcd619ac278aa08ea38fc448a65cbcf
septs <github@septs.pw>
2024-06-21 11:37:19 +0800

    refactor: based-cond setup debian

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -xeuo pipefail
 sudo apt-get update
-sudo apt-get install -y build-essential libpcsclite-dev libcurl4-openssl-dev gcc-mingw-w64 g++-mingw-w64 zip
+sudo apt-get install -y build-essential libpcsclite-dev libcurl4-openssl-dev zip
+
+case "${1:-}" in
+mingw)
+    sudo apt-get install gcc-mingw-w64 g++-mingw-w64
+    ;;
+esac
