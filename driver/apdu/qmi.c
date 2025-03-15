estkme-group/lpac

commit 3bde4a1d3dd6ada280ac3b3e53a6b65997ca8f27

driver(APDU): add QMI backend (#131)
* driver: pass EUICC APDU or HTTP struct to driver fini OP

Currently, the .fini driver OP can only be used with global variables, but
that will be an issue when we cant use global variables.

This will be used with direct QMI in order to free the memory used by the
driver private structure instead of global variables.

Signed-off-by: Robert Marko <robert.marko@sartura.hr>

* driver: apdu: rename QRTR QMI helpers to QMI helpers

Almost all of the QRTR QMI helpers will be reused for direct QMI so,
lets rename the sources to indicate that.

Signed-off-by: Robert Marko <robert.marko@sartura.hr>

* driver: apdu: extract common QMI code

Support for using QMI directly and not over QRTR would use a lot of the
same code as QMI over QRTR, so in order to prevent code duplication lets
extract that common code so it can be reused.

Signed-off-by: Robert Marko <robert.marko@sartura.hr>

* driver: apdu: qmi-helpers: allow compiling without libqrtr

Direct QMI backend wont use any of the libqmi QRTR functionality so it
wont depend on libqrtr and thus we need to make sure QMI helpers still
compile without libqrtr in the system.

Signed-off-by: Robert Marko <robert.marko@sartura.hr>

* driver: apdu: dont use global variables for QMI

Since we split out the common QMI code then we cannot be using global
variables since existing QRTR and the coming QMI drivers will clash
by trying to use the same global variable names with the common code.

So, lets move to passing a structure which is driver specific instead.

Since we are not using global variables anymore, we cannot be using atexit
anymore, so lets move that cleanup step to driver finish OP instead.

Signed-off-by: Robert Marko <robert.marko@sartura.hr>

* driver: apdu: add QMI backend

Previously QMI over QRTR support was added, however that is only present in
modern Qualcomm modems and only when running in PCIe mode and its quite

driver/apdu/qmi.c
@@ -1,73 +1,43 @@
 // SPDX-License-Identifier: MIT
 /*
- * Copyright (c) 2024, Luca Weiss <luca.weiss@fairphone.com>
+ * Copyright (c) 2024, Robert Marko <robert.marko@sartura.hr>
  */
-#include "qmi_qrtr.h"
+#include "qmi.h"
 
-#include <euicc/interface.h>
-#include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <libqrtr-glib.h>
 #include "qmi_common.h"
-
-static QrtrBus *bus = NULL;
 
 static int apdu_interface_connect(struct euicc_ctx *ctx)
 {
     struct qmi_data *qmi_priv = ctx->apdu.interface->userdata;
     g_autoptr(GError) error = NULL;
-    QrtrNode *node = NULL;
     QmiDevice *device = NULL;
     QmiClient *client = NULL;
-    bool found = false;
+    const char *device_path;
+    GFile *file;
+
+    if (!(device_path = getenv("QMI_DEVICE"))) {
+        fprintf(stderr, "No QMI device path specified!\n");
+        return -1;
+    }
+    file = g_file_new_for_path(device_path);
 
     qmi_priv->context = g_main_context_new();
 
-    bus = qrtr_bus_new_sync(qmi_priv->context, &error);
-    if (bus == NULL)
-    {
-        fprintf(stderr, "error: connect to QRTR bus failed: %s\n", error->message);
-        return -1;
-    }
-
-    /* Find QRTR node for UIM service */
-    for (GList *l = qrtr_bus_peek_nodes(bus); l != NULL; l = l->next)
-    {
-        node = l->data;
-
-        if (node && qrtr_node_lookup_port(node, QMI_SERVICE_UIM) >= 0)
-        {
-            found = true;
-            break;
-        }
-    }
-
-    if (!found)
-    {
-        fprintf(stderr, "error: find QRTR node with UIM service failed\n");
-        return -1;
-    }
-
-    device = qmi_device_new_from_node_sync(node, qmi_priv->context, &error);
-    if (!device)
-    {
-        fprintf(stderr, "error: create QMI device from QRTR node failed: %s\n", error->message);
+    device = qmi_device_new_from_path(file, qmi_priv->context, &error);
+    if (!device) {
+        fprintf(stderr, "error: create QMI device from path failed: %s\n", error->message);
         return -1;
     }
 
     qmi_device_open_sync(device, qmi_priv->context, &error);
-    if (error)
-    {
+    if (error) {
         fprintf(stderr, "error: open QMI device failed: %s\n", error->message);
         return -1;
     }
 
     client = qmi_device_allocate_client_sync(device, qmi_priv->context, &error);
-    if (!client)
-    {
+    if (!client) {
         fprintf(stderr, "error: allocate QMI client failed: %s\n", error->message);
         return -1;
     }
@@ -88,7 +58,6 @@ static int libapduinterface_init(struct euicc_apdu_interface *ifstruct)
     }
 
     memset(ifstruct, 0, sizeof(struct euicc_apdu_interface));
-
     ifstruct->connect = apdu_interface_connect;
     ifstruct->disconnect = qmi_apdu_interface_disconnect;
     ifstruct->logic_channel_open = qmi_apdu_interface_logic_channel_open;
@@ -96,17 +65,13 @@ static int libapduinterface_init(struct euicc_apdu_interface *ifstruct)
     ifstruct->transmit = qmi_apdu_interface_transmit;
 
     /*
-     * Allow the user to select the SIM card slot via environment variable.
-     * Use the primary SIM slot if not set.
-     */
+    * Allow the user to select the SIM card slot via environment variable.
+    * Use the primary SIM slot if not set.
+    */
     if (getenv("UIM_SLOT"))
-    {
         qmi_priv->uimSlot = atoi(getenv("UIM_SLOT"));
-    }
     else
-    {
         qmi_priv->uimSlot = 1;
-    }
 
     ifstruct->userdata = qmi_priv;
 
@@ -127,9 +92,9 @@ static void libapduinterface_fini(struct euicc_apdu_interface *ifstruct)
     free(qmi_priv);
 }
 
-const struct euicc_driver driver_apdu_qmi_qrtr = {
+const struct euicc_driver driver_apdu_qmi = {
     .type = DRIVER_APDU,
-    .name = "qmi_qrtr",
+    .name = "qmi",
     .init = (int (*)(void *))libapduinterface_init,
     .main = libapduinterface_main,
     .fini = (void (*)(void *))libapduinterface_fini,

driver/apdu/qmi_helpers.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2024, Luca Weiss <luca.weiss@fairphone.com>
  */
 
-#include "qmi_qrtr_helpers.h"
+#include "qmi_helpers.h"
 
 static void
 async_result_ready(GObject *source_object,
@@ -16,6 +16,7 @@ async_result_ready(GObject *source_object,
     *result_out = g_object_ref(res);
 }
 
+#ifdef LPAC_WITH_APDU_QMI_QRTR
 QrtrBus *
 qrtr_bus_new_sync(GMainContext *context,
                   GError **error)
@@ -56,6 +57,33 @@ qmi_device_new_from_node_sync(QrtrNode *node,
 
     return qmi_device_new_from_node_finish(result, error);
 }
+#endif
+
+#ifdef LPAC_WITH_APDU_QMI
+QmiDevice *
+qmi_device_new_from_path(GFile *file,
+                         GMainContext *context,
+                         GError **error)
+{
+    g_autoptr(GMainContextPusher) pusher = NULL;
+    g_autoptr(GAsyncResult) result = NULL;
+    g_autofree gchar *id = NULL;
+
+    pusher = g_main_context_pusher_new(context);
+
+    id = g_file_get_path (file);
+    if (id)
+        qmi_device_new(file,
+                       NULL,
+                       async_result_ready,
+                       &result);
+
+    while (result == NULL)
+        g_main_context_iteration(context, TRUE);
+
+    return qmi_device_new_finish(result, error);
+}
+#endif
 
 gboolean
 qmi_device_open_sync(QmiDevice *device,
@@ -68,7 +96,7 @@ qmi_device_open_sync(QmiDevice *device,
     pusher = g_main_context_pusher_new(context);
 
     qmi_device_open(device,
-                    QMI_DEVICE_OPEN_FLAGS_NONE,
+                    QMI_DEVICE_OPEN_FLAGS_PROXY,
                     15,
                     NULL,
                     async_result_ready,

driver/apdu/qmi_helpers.h
@@ -3,8 +3,10 @@
  * Copyright (c) 2024, Luca Weiss <luca.weiss@fairphone.com>
  */
 
-#include <libqrtr-glib.h>
 #include <libqmi-glib.h>
+
+#ifdef LPAC_WITH_APDU_QMI_QRTR
+#include <libqrtr-glib.h>
 
 QrtrBus *qrtr_bus_new_sync(
     GMainContext *context,
@@ -15,6 +17,15 @@ qmi_device_new_from_node_sync(
     QrtrNode *node,
     GMainContext *context,
     GError **error);
+#endif
+
+#ifdef LPAC_WITH_APDU_QMI
+QmiDevice *
+qmi_device_new_from_path(
+    GFile *file,
+    GMainContext *context,
+    GError **error);
+#endif
 
 gboolean
 qmi_device_open_sync(

driver/CMakeLists.txt
@@ -4,6 +4,7 @@ cmake_dependent_option(LPAC_DYNAMIC_DRIVERS "Build lpac/libeuicc driver backends
 option(LPAC_WITH_APDU_PCSC "Build APDU PCSC Backend (requires PCSC libraries)" ON)
 option(LPAC_WITH_APDU_AT "Build APDU AT Backend" ON)
 option(LPAC_WITH_APDU_GBINDER "Build APDU Gbinder backend for libhybris devices (requires gbinder headers)" OFF)
+option(LPAC_WITH_APDU_QMI "Build QMI backend for Qualcomm devices (requires libqmi)" OFF)
 option(LPAC_WITH_APDU_QMI_QRTR "Build QMI-over-QRTR backend for Qualcomm devices (requires libqrtr and libqmi headers)" OFF)
 
 option(LPAC_WITH_HTTP_CURL "Build HTTP Curl interface" ON)
@@ -59,9 +60,26 @@ if(LPAC_WITH_APDU_GBINDER)
     endif()
 endif()
 
+if(LPAC_WITH_APDU_QMI)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DLPAC_WITH_APDU_QMI")
+    target_sources(euicc-drivers PRIVATE
+    ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi_helpers.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi_common.c)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(QMI_GLIB REQUIRED IMPORTED_TARGET qmi-glib)
+    target_link_libraries(euicc-drivers PkgConfig::QMI_GLIB)
+    if(LPAC_DYNAMIC_DRIVERS)
+        list(APPEND LIBEUICC_DRIVERS_REQUIRES "qmi-glib")
+    endif()
+endif()
+
 if(LPAC_WITH_APDU_QMI_QRTR)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DLPAC_WITH_APDU_QMI_QRTR")
-    target_sources(euicc-drivers PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi_qrtr.c ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi_qrtr_helpers.c)
+    target_sources(euicc-drivers PRIVATE
+    ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi_qrtr.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi_helpers.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/apdu/qmi_common.c)
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(QRTR_GLIB REQUIRED IMPORTED_TARGET qrtr-glib)
     pkg_check_modules(QMI_GLIB REQUIRED IMPORTED_TARGET qmi-glib)

driver/apdu/at.c
@@ -218,7 +218,7 @@ static int libapduinterface_main(int argc, char **argv)
     return 0;
 }
 
-static void libapduinterface_fini(void)
+static void libapduinterface_fini(struct euicc_apdu_interface *ifstruct)
 {
 }
 
@@ -227,5 +227,5 @@ const struct euicc_driver driver_apdu_at = {
     .name = "at",
     .init = (int (*)(void *))libapduinterface_init,
     .main = libapduinterface_main,
-    .fini = libapduinterface_fini,
+    .fini = (void (*)(void *))libapduinterface_fini,
 };

driver/apdu/gbinder_hidl.c
@@ -314,7 +314,7 @@ static int libapduinterface_main(int argc, char **argv)
     return 0;
 }
 
-static void libapduinterface_fini(void)
+static void libapduinterface_fini(struct euicc_apdu_interface *ifstruct)
 {
 }
 

driver/apdu/pcsc.c
@@ -453,7 +453,7 @@ static int libapduinterface_main(int argc, char **argv)
     return 0;
 }
 
-static void libapduinterface_fini(void)
+static void libapduinterface_fini(struct euicc_apdu_interface *ifstruct)
 {
 }
 
@@ -462,5 +462,5 @@ const struct euicc_driver driver_apdu_pcsc = {
     .name = "pcsc",
     .init = (int (*)(void *))libapduinterface_init,
     .main = libapduinterface_main,
-    .fini = libapduinterface_fini,
+    .fini = (void (*)(void *))libapduinterface_fini,
 };

driver/apdu/qmi.h
@@ -1,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright (c) 2024, Robert Marko <robert.marko@sartura.hr>
+ */
+#pragma once
+#include <driver.private.h>
+
+extern const struct euicc_driver driver_apdu_qmi;

commit 1c85febf7dedd4be741571ca63c587044c823cd2

driver: apdu: qmi*: Pass correct open flag for QRTR driver
While the QMI driver which goes to /dev/foobar wants to go through the
qmi-proxy to not have to exclusively claim the device, the QMI-over-QRTR
driver doesn't work with the PROXY flag and just leads to the error

  error: open QMI device failed: endpoint hangup

Fix this by passing the correct flag to qmi_device_open from both APDU
drivers.

Closes: #215
Fixes: 3bde4a1 (" driver(APDU): add QMI backend (#131)")

driver/apdu/qmi.c
@@ -30,7 +30,7 @@ static int apdu_interface_connect(struct euicc_ctx *ctx)
         return -1;
     }
 
-    qmi_device_open_sync(device, qmi_priv->context, &error);
+    qmi_device_open_sync(device, QMI_DEVICE_OPEN_FLAGS_PROXY, qmi_priv->context, &error);
     if (error) {
         fprintf(stderr, "error: open QMI device failed: %s\n", error->message);
         return -1;

driver/apdu/qmi_helpers.c
@@ -87,6 +87,7 @@ qmi_device_new_from_path(GFile *file,
 
 gboolean
 qmi_device_open_sync(QmiDevice *device,
+                     QmiDeviceOpenFlags flags,
                      GMainContext *context,
                      GError **error)
 {
@@ -96,7 +97,7 @@ qmi_device_open_sync(QmiDevice *device,
     pusher = g_main_context_pusher_new(context);
 
     qmi_device_open(device,
-                    QMI_DEVICE_OPEN_FLAGS_PROXY,
+                    flags,
                     15,
                     NULL,
                     async_result_ready,

driver/apdu/qmi_helpers.h
@@ -30,6 +30,7 @@ qmi_device_new_from_path(
 gboolean
 qmi_device_open_sync(
     QmiDevice *device,
+    QmiDeviceOpenFlags flags,
     GMainContext *context,
     GError **error);
 

driver/apdu/qmi_qrtr.c
@@ -58,7 +58,7 @@ static int apdu_interface_connect(struct euicc_ctx *ctx)
         return -1;
     }
 
-    qmi_device_open_sync(device, qmi_priv->context, &error);
+    qmi_device_open_sync(device, QMI_DEVICE_OPEN_FLAGS_NONE, qmi_priv->context, &error);
     if (error)
     {
         fprintf(stderr, "error: open QMI device failed: %s\n", error->message);