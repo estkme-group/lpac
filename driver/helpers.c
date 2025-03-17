988ccd5938a671225425be56aa4e594bc42a4ffe
septs <github@septs.pw>
2025-03-17 22:56:28 +0800

    chore: add setenv func

--- driver/helpers.c
+++ driver/helpers.c
@@ -1,5 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+
+#ifdef _WIN32
+int setenv(const char *name, const char *value, const int overwrite) {
+    if (overwrite) goto replace;
+    size_t n = 0;
+    const int errcode = getenv_s(&n, NULL, 0, name);
+    if (errcode || n) return errcode;
+replace:
+    return _putenv_s(name, value);
+}
+#endif
 
 void set_deprecated_env_name(const char *name, const char *deprecated_name) {
     const char *value = getenv(name);
@@ -7,9 +19,5 @@ void set_deprecated_env_name(const char *name, const char *deprecated_name) {
     value = getenv(deprecated_name);
     if (value == NULL) return; // deprecated env var not set
     fprintf(stderr, "WARNING: Please use '%s' instead of '%s'\n", name, deprecated_name);
-#ifdef _WIN32
-    _putenv_s(name, value);
-#else
-    setenv(name, value, 1);
-#endif
+    setenv(name, value, 0);
 }
--- driver/helpers.h
+++ driver/helpers.h
@@ -3,4 +3,8 @@
 #define HTTP_ENV_NAME(DRIVER, NAME) "LPAC_HTTP_" #DRIVER "_" #NAME
 #define APDU_ENV_NAME(DRIVER, NAME) "LPAC_APDU_" #DRIVER "_" #NAME
 
+#ifdef _WIN32
+int setenv(const char *name, const char *value, int overwrite);
+#endif
+
 void set_deprecated_env_name(const char *name, const char *deprecated_name);
