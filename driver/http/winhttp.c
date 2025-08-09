bccc5ed453dcf60d0744999ad5691af4606f408b
septs <github@septs.pw>
2025-08-09 20:38:11 +0800

    refactor: winhttp

--- driver/http/winhttp.c
+++ driver/http/winhttp.c
@@ -10,120 +10,99 @@
 
 #include <euicc/interface.h>
 
-static wchar_t *toLPCWSTR(const char *input) {
-    const int n = MultiByteToWideChar(CP_ACP, 0, input, -1, NULL, 0);
-    wchar_t *output = calloc(sizeof(wchar_t), n);
-    MultiByteToWideChar(CP_ACP, 0, input, -1, output, 4096);
+wchar_t *utf8_to_wide(const char *input) {
+    const int len = MultiByteToWideChar(CP_UTF8, 0, input, -1, NULL, 0);
+    if (len == 0) return NULL;
+    wchar_t *output = (wchar_t *) malloc(len * sizeof(wchar_t));
+    if (!output) return NULL;
+    MultiByteToWideChar(CP_UTF8, 0, input, -1, output, len);
     return output;
-}
-
-static int toUrlComponents(const char *url, URL_COMPONENTS *urlComponents) {
-    memset(urlComponents, 0, sizeof(URL_COMPONENTS));
-    urlComponents->dwStructSize = sizeof(URL_COMPONENTS);
-    urlComponents->dwHostNameLength = -1;
-    urlComponents->dwUrlPathLength = -1;
-    wchar_t *wUrl = toLPCWSTR(url);
-    const int ret = WinHttpCrackUrl(wUrl, 0, 0, urlComponents);
-    free(wUrl);
-    return ret;
 }
 
 static int http_interface_transmit(struct euicc_ctx *ctx, const char *url, uint32_t *rcode, uint8_t **rx,
                                    uint32_t *rx_len, const uint8_t *tx, uint32_t tx_len, const char **h) {
     int fret = 0;
-    DWORD dwSize = 0;
-    DWORD dwDownloaded = 0;
-    URL_COMPONENTS urlComponents;
     HINTERNET hSession = NULL, hConnect = NULL, hRequest = NULL;
     BOOL bResults = FALSE;
 
-    if (toUrlComponents(url, &urlComponents) != 0) {
-        fret = -1;
-        goto exit;
-    }
+    URL_COMPONENTS urlComp = {0};
+    ZeroMemory(&urlComp, sizeof(urlComp));
+    urlComp.dwStructSize = sizeof(urlComp);
+    urlComp.dwHostNameLength = 256;
+    urlComp.lpszHostName = calloc(urlComp.dwHostNameLength, sizeof(WCHAR));
+    urlComp.dwUrlPathLength = 1024;
+    urlComp.lpszUrlPath = calloc(urlComp.dwUrlPathLength, sizeof(WCHAR));
+
+    wchar_t *wUrl = utf8_to_wide(url);
+    if (!WinHttpCrackUrl(wUrl, 0, 0, &urlComp)) goto exit;
+    free(wUrl);
+
     // Open a session
-    {
-        hSession = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS,
-                               0);
-        if (hSession == NULL) goto error;
-    }
+    hSession = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS,
+                           0);
+    if (hSession == NULL) goto error;
+    // Connect to the server
+    hConnect = WinHttpConnect(hSession, urlComp.lpszHostName, INTERNET_DEFAULT_HTTPS_PORT, 0);
+    if (hConnect == NULL) goto error;
+    // Open Request
+    hRequest = WinHttpOpenRequest(hConnect, L"POST", urlComp.lpszUrlPath, NULL, WINHTTP_NO_REFERER,
+                                  WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_SECURE);
+    if (hRequest == NULL) goto error;
     // Set the SSL options
     {
-        DWORD dwOptions = SECURITY_FLAG_IGNORE_UNKNOWN_CA;
-        bResults = WinHttpSetOption(hSession, WINHTTP_OPTION_SECURITY_FLAGS, &dwOptions, sizeof(DWORD));
-        if (bResults == FALSE) goto error;
-    }
-    // Connect to the server
-    {
-        hConnect = WinHttpConnect(hSession, urlComponents.lpszHostName, INTERNET_DEFAULT_HTTPS_PORT, 0);
-        if (hConnect == NULL) goto error;
-    }
-    // Open Request
-    {
-        hRequest = WinHttpOpenRequest(hConnect, L"POST", urlComponents.lpszUrlPath, NULL, WINHTTP_NO_REFERER,
-                                      WINHTTP_DEFAULT_ACCEPT_TYPES,
-                                      WINHTTP_FLAG_SECURE);
-        if (hRequest == NULL) goto error;
-    }
-    // Add HTTP header to hRequest
-    {
-        wchar_t *header = NULL;
-        for (int i = 0; h[i] != NULL; i++) {
-            header = toLPCWSTR(h[i]);
-            bResults = WinHttpAddRequestHeaders(hRequest, header, (ULONG) -1L, WINHTTP_ADDREQ_FLAG_ADD);
-            if (bResults == FALSE) goto error;
-        }
-        free(header);
+        DWORD dwFlags = SECURITY_FLAG_IGNORE_UNKNOWN_CA;
+        bResults = WinHttpSetOption(hRequest, WINHTTP_OPTION_SECURITY_FLAGS, &dwFlags, sizeof(DWORD));
+        if (!bResults) goto error;
+        bResults = WinHttpSetOption(hRequest, WINHTTP_OPTION_CLIENT_CERT_CONTEXT, WINHTTP_NO_CLIENT_CERT_CONTEXT, 0);
+        if (!bResults) goto error;
     }
     // Send Request
     {
-        wchar_t *body = toLPCWSTR((const char *) tx);
-        bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, body, tx_len, tx_len, 0);
-        free(body);
-        if (bResults == FALSE) goto error;
+        char *headers = strdup(h[0]);
+        for (int i = 1; h[i] != NULL; i++) {
+            headers = strcat(headers, "\r\n");
+            headers = strcat(headers, h[i]);
+        }
+        wchar_t *wHeaders = utf8_to_wide(headers);
+        wchar_t *wPayload = utf8_to_wide((const char *) tx);
+        bResults = WinHttpSendRequest(hRequest, wHeaders, -1L, (LPVOID) wPayload, tx_len, tx_len, 0);
+        free(wHeaders);
+        free(wPayload);
+        if (!bResults) goto error;
     }
     // Receive Response
-    {
-        bResults = WinHttpReceiveResponse(hRequest, NULL);
-        if (bResults == FALSE) goto error;
-    }
+    bResults = WinHttpReceiveResponse(hRequest, NULL);
+    if (!bResults) goto error;
     // Get the response status code
+    bResults = WinHttpQueryHeaders(hRequest, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER,
+                                   WINHTTP_HEADER_NAME_BY_INDEX, &rcode, NULL, WINHTTP_NO_HEADER_INDEX);
+    if (!bResults) goto error;
+    // Read the response data
     {
-        bResults = WinHttpQueryHeaders(hRequest, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER,
-                                       WINHTTP_HEADER_NAME_BY_INDEX,
-                                       &rcode, NULL, WINHTTP_NO_HEADER_INDEX);
-        if (bResults == FALSE) goto error;
+        *rx = NULL;
+        *rx_len = 0;
+
+        DWORD dwSize = 0;
+        DWORD dwDownloaded = 0;
+        do {
+            if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) break;
+            if (dwSize == 0) break;
+            char *pszOutBuffer = malloc(dwSize + 1);
+            if (!pszOutBuffer) break;
+            if (!WinHttpReadData(hRequest, (LPVOID) pszOutBuffer, dwSize, &dwDownloaded)) break;
+            pszOutBuffer[dwDownloaded] = '\0';
+            printf("%s", pszOutBuffer);
+            free(pszOutBuffer);
+        } while (dwSize > 0);
+
+        // *rx = buffer;
+        // *rx_len = totalSize;
     }
-
-    *rx = NULL;
-    *rx_len = 0;
-
-    LPSTR pszOutBuffer;
-    uint8_t *rx_buffer = NULL;
-    uint32_t rx_offset = 0;
-    do {
-        dwSize = 0;
-        if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) goto error;
-        pszOutBuffer = malloc(dwSize + 1);
-        if (pszOutBuffer == NULL) goto error;
-        memset(pszOutBuffer, 0, dwSize + 1);
-        if (!WinHttpReadData(hRequest, pszOutBuffer, dwSize, &dwDownloaded)) goto error;
-
-        rx_buffer = realloc(rx_buffer, dwDownloaded);
-        memcpy(rx_buffer + rx_offset, rx, dwSize);
-        rx_offset += dwSize;
-
-        free(pszOutBuffer);
-
-        if (!dwDownloaded) break;
-    } while (dwSize > 0);
-
-    *rx = rx_buffer;
-    *rx_len = dwDownloaded;
 
     goto exit;
 error:
     fret = (int) GetLastError();
+    printf("Error: %d\n", fret);
 exit:
     if (hRequest) WinHttpCloseHandle(hRequest);
     if (hConnect) WinHttpCloseHandle(hConnect);
