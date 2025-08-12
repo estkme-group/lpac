9793fabdf2f048311edaafdebed048a451857aad
septs <github@septs.pw>
2025-08-12 17:53:14 +0800

    chore: simplify setup qmi

--- scripts/setup-qmi.sh
+++ scripts/setup-qmi.sh
@@ -9,18 +9,17 @@ apt-get -qq --no-install-recommends install -y libqrtr-glib-dev libmbim-glib-dev
 
 TMPDIR="$(mktemp -d)"
 
-function download() {
-    local NAME="$1"
-    curl -L "https://launchpad.net/ubuntu/+archive/primary/+files/$NAME" -o "$TMPDIR/$NAME"
-}
-
 # https://launchpad.net/libqmi
-VERSION="1.36.0-1_$(dpkg --print-architecture)"
+QMI_VERSION="1.36.0-1_$(dpkg --print-architecture)"
 
-download "libqmi-glib5_$VERSION.deb"
-download "libqmi-glib-dev_$VERSION.deb"
-download "libqmi-utils_$VERSION.deb"
-download "libqmi-proxy_$VERSION.deb"
-download "gir1.2-qmi-1.0_$VERSION.deb"
+wget -nv -P "$TMPDIR" -i - <<EOF
+https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-glib5_$QMI_VERSION.deb
+https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-glib-dev_$QMI_VERSION.deb
+https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-utils_$QMI_VERSION.deb
+https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-proxy_$QMI_VERSION.deb
+https://launchpad.net/ubuntu/+archive/primary/+files/gir1.2-qmi-1.0_$QMI_VERSION.deb
+EOF
 
 dpkg -i "$TMPDIR"/*
+
+rm -vrf "$TMPDIR"
