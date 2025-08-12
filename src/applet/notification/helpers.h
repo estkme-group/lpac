54228325d4b662787045f4a1b1d68cae40abc2fc
septs <github@septs.pw>
2025-08-12 14:13:28 +0800

    feat(notification): replay (#257)
    * feat: notification replay
    
    * fix(replay): enforce input redirection for applet execution
    
    * chore: aceept reviews
    
    * chore: aceept reviews
    
    * Apply suggestions from code review
    
    Co-authored-by: Coelacanthus <uwu@coelacanthus.name>
    
    * chore: remove unused headers
    
    * fix: build failed
    
    ---------
    
    Co-authored-by: Coelacanthus <uwu@coelacanthus.name>

--- src/applet/notification/helpers.h
+++ src/applet/notification/helpers.h
@@ -1,3 +1,13 @@
 #pragma once
 
+#include <stdbool.h>
+
+#include <cjson/cJSON.h>
+#include <euicc/es10b.h>
+
 char *notification_strstrip(char *input);
+
+cJSON *build_notification(const char *eid, uint32_t seqNumber, const struct es10b_pending_notification *notification);
+
+bool parse_notification(const cJSON *jroot, const char *eid, uint32_t *seqNumber,
+                        struct es10b_pending_notification *notification);
