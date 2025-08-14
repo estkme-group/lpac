fb42029d55a624c4eeacc9503a0d34ca0547c650
septs <github@septs.pw>
2025-08-14 17:29:03 +0800

    fix(euicc): update uicc capabilities
    Co-authored-by: sekaiacg <sekaiacg@gmail.com>

--- euicc/es10c_ex.c
+++ euicc/es10c_ex.c
@@ -84,7 +84,14 @@ int es10c_ex_get_euiccinfo2(struct euicc_ctx *ctx, struct es10c_ex_euiccinfo2 *e
             break;
         case 0x85: // uiccCapability
         {
-            static const char *desc[] = {"contactlessSupport", "usimSupport", "isimSupport", "csimSupport", "akaMilenage", "akaCave", "akaTuak128", "akaTuak256", "rfu1", "rfu2", "gbaAuthenUsim", "gbaAuthenISim", "mbmsAuthenUsim", "eapClient", "javacard", "multos", "multipleUsimSupport", "multipleIsimSupport", "multipleCsimSupport", NULL};
+            static const char *desc[] = {
+                "contactlessSupport", "usimSupport", "isimSupport", "csimSupport", "akaMilenage", "akaCave",
+                "akaTuak128", "akaTuak256", "rfu1", "rfu2", "gbaAuthenUsim", "gbaAuthenISim", "mbmsAuthenUsim",
+                "eapClient", "javacard", "multos", "multipleUsimSupport", "multipleIsimSupport", "multipleCsimSupport",
+                "berTlvFileSupport", "dfLinkSupport", "catTp", "getIdentity", "profile-a-x25519", "profile-b-p256",
+                "suciCalculatorApi",
+                NULL
+            };
 
             if (euicc_derutil_convert_bin2bits_str(&euiccinfo2->uiccCapability, tmpnode.value, tmpnode.length, desc))
             {
