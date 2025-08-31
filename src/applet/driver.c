daa1ed98c35e574d3327ffe94a5ffbf879ff02a1
septs <github@septs.pw>
2025-08-31 18:39:34 +0800

    format(driver): improve code formatting for applet_entry definitions

--- src/applet/driver.c
+++ src/applet/driver.c
@@ -4,12 +4,26 @@
 #include <jprint.h>
 #include <stddef.h>
 
-const struct applet_entry applet_apdu = {.name = "apdu", .main = euicc_driver_main_apdu, .skip_init_euicc = true};
+// clang-format off
+const struct applet_entry applet_apdu = {
+    .name = "apdu",
+    .main = euicc_driver_main_apdu,
+    .skip_init_euicc = true
+};
 
-const struct applet_entry applet_http = {.name = "http", .main = euicc_driver_main_http, .skip_init_euicc = true};
+const struct applet_entry applet_http = {
+    .name = "http",
+    .main = euicc_driver_main_http,
+    .skip_init_euicc = true
+};
 
 const struct applet_entry applet_list = {
-    .name = "list", .main = euicc_driver_list, .skip_init_driver = true, .skip_init_euicc = true};
+    .name = "list",
+    .main = euicc_driver_list,
+    .skip_init_driver = true,
+    .skip_init_euicc = true
+};
+// clang-format on
 
 const struct applet_entry *applets[] = {
     &applet_apdu,
