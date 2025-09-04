83c71781373df9cb6e0359d5e19455a81eae0823
septs <github@septs.pw>
2025-09-04 17:04:53 +0800

    refactor(logger): rename print_hex to euicc_hex_print and update logging format

--- euicc/logger.c
+++ euicc/logger.c
@@ -12,10 +12,7 @@ static void bash_colorize(FILE *fp, const int color) {
         fprintf(fp, "\033[%dm", color);
 }
 
-static void print_hex(FILE *fp, const uint8_t *data, const uint32_t length) {
-    if (length == 0)
-        return;
-    fputs(", Data:", fp);
+static void euicc_hex_print(FILE *fp, const uint8_t *data, const uint32_t length) {
     for (uint32_t i = 0; i < length; i++)
         fprintf(fp, " %02X", data[i] & 0xFF);
 }
@@ -24,9 +21,9 @@ inline void euicc_apdu_request_print(FILE *fp, const struct apdu_request *req, c
     if (fp == NULL)
         return;
     bash_colorize(fp, BASH_GREEN);
-    fprintf(fp, "[DEBUG] [APDU] [TX] CLA: %02X, INS: %02X, P1: %02X, P2: %02X, Lc: %02X", req->cla, req->ins, req->p1,
-            req->p2, req->length);
-    print_hex(fp, req->data, req_len - sizeof(struct apdu_request));
+    fprintf(fp, "[DEBUG] [APDU] [TX] CLA: %02X, INS: %02X, P1: %02X, P2: %02X, Lc: %02X, Data:", req->cla, req->ins,
+            req->p1, req->p2, req->length);
+    euicc_hex_print(fp, req->data, req_len - sizeof(struct apdu_request));
     bash_colorize(fp, BASH_CLEAR);
     fputc('\n', fp);
 }
@@ -35,8 +32,8 @@ inline void euicc_apdu_response_print(FILE *fp, const struct apdu_response *resp
     if (fp == NULL)
         return;
     bash_colorize(fp, BASH_RED);
-    fprintf(fp, "[DEBUG] [APDU] [RX] SW1: %02X, SW2: %02X", resp->sw1, resp->sw2);
-    print_hex(fp, resp->data, resp->length);
+    fprintf(fp, "[DEBUG] [APDU] [RX] SW1: %02X, SW2: %02X, Data:", resp->sw1, resp->sw2);
+    euicc_hex_print(fp, resp->data, resp->length);
     bash_colorize(fp, BASH_CLEAR);
     fputc('\n', fp);
 }
@@ -45,9 +42,7 @@ inline void euicc_http_request_print(FILE *fp, const char *url, const char *tx) 
     if (fp == NULL)
         return;
     bash_colorize(fp, BASH_GREEN);
-    fprintf(fp, "[DEBUG] [HTTP] [TX] URL: %s", url);
-    if (strlen(tx) > 0)
-        fprintf(fp, ", Data: %s", tx);
+    fprintf(fp, "[DEBUG] [HTTP] [TX] URL: %s, Data: %s", url, tx);
     bash_colorize(fp, BASH_CLEAR);
     fputc('\n', fp);
 }
@@ -56,9 +51,7 @@ inline void euicc_http_response_print(FILE *fp, const uint32_t rcode, const char
     if (fp == NULL)
         return;
     bash_colorize(fp, BASH_RED);
-    fprintf(fp, "[DEBUG] [HTTP] [RX] RCode: %d", rcode);
-    if (strlen(rx) > 0)
-        fprintf(fp, ", Data: %s", rx);
+    fprintf(fp, "[DEBUG] [HTTP] [RX] RCode: %d, Data: %s", rcode, rx);
     bash_colorize(fp, BASH_CLEAR);
     fputc('\n', fp);
 }
@@ -69,8 +62,7 @@ inline void euicc_unhandled_tag_print(FILE *fp, const struct euicc_derutil_node 
     fputc('\n', fp);
     bash_colorize(fp, BASH_RED);
     fprintf(fp, "[PLEASE REPORT] [TODO] [TAG %02X]:", node->tag);
-    for (uint32_t i = 0; i < node->self.length; i++)
-        fprintf(fp, " %02X", node->self.ptr[i]);
+    euicc_hex_print(fp, node->self.ptr, node->self.length);
     bash_colorize(fp, BASH_CLEAR);
     fputc('\n', fp);
 }
