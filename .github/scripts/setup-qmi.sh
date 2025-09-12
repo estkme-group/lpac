654af3c261d305403fe6fabe6120ad5bfa9df8e8
septs <github@septs.pw>
2025-09-12 12:00:35 +0800

    chore(ci): libqmi from source code build

--- .github/scripts/setup-qmi.sh
+++ .github/scripts/setup-qmi.sh
@@ -7,21 +7,25 @@ function apt() {
         apt-get -qq -o=Dpkg::Use-Pty=0 "$@"
 }
 
-apt install -y libqrtr-glib-dev libmbim-glib-dev
+apt install -y meson ninja-build \
+    libgirepository1.0-dev libglib2.0-dev libgudev-1.0-dev \
+    libqrtr-glib-dev libmbim-glib-dev
 
-TMPDIR="$(mktemp -d)"
+# QMI_REPO_URL="https://gitlab.freedesktop.org/mobile-broadband/libqmi.git" # official repo
+QMI_REPO_URL="https://github.com/linux-mobile-broadband/libqmi" # official mirror repo
+QMI_REPO_DIR="$(mktemp -d libqmi-src-XXXXXX)"
+QMI_BUILD_DIR="$QMI_REPO_DIR/build"
+QMI_VERSION=1.35.5-dev
 
-trap 'rm -vrf '"$TMPDIR" EXIT
+trap 'rm -rf '"$QMI_REPO_DIR" EXIT
 
-# https://launchpad.net/libqmi
-QMI_VERSION="1.36.0-1_$(dpkg --print-architecture)"
-
-wget -nv -P "$TMPDIR" -i - <<EOF
-https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-glib5_$QMI_VERSION.deb
-https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-glib-dev_$QMI_VERSION.deb
-https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-utils_$QMI_VERSION.deb
-https://launchpad.net/ubuntu/+archive/primary/+files/libqmi-proxy_$QMI_VERSION.deb
-https://launchpad.net/ubuntu/+archive/primary/+files/gir1.2-qmi-1.0_$QMI_VERSION.deb
-EOF
-
-sudo dpkg -i "$TMPDIR"/*
+pushd "$QMI_REPO_DIR"
+wget -nv "$QMI_REPO_URL/archive/refs/tags/$QMI_VERSION.tar.gz" -O - |
+    tar xz --strip-components=1
+meson setup "$QMI_BUILD_DIR" --prefix=/usr --buildtype=release \
+    -Dman=false -Dbash_completion=false
+pushd "$QMI_BUILD_DIR"
+ninja -j"$(nproc)"
+sudo ninja install
+popd
+popd
