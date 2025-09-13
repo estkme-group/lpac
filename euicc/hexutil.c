660611f5dce91fa413a927bef24706e8532f0d19
septs <github@septs.pw>
2025-09-13 16:54:39 +0800

    chore: accept reviews

--- euicc/hexutil.c
+++ euicc/hexutil.c
@@ -8,6 +8,23 @@
 #    include <stddef.h>
 #endif
 
+#define SWAP(a, b)            \
+    do {                      \
+        typeof(a) temp = (a); \
+        (a) = (b);            \
+        (b) = temp;           \
+    } while (0)
+
+static void gsmbcd_swap_chars(char *input, const uint32_t input_len) {
+    for (uint32_t i = 0; i < input_len; i += 2) {
+        SWAP(input[i], input[i + 1]);
+    }
+}
+
+inline int euicc_hexutil_hex2bin(uint8_t *output, const uint32_t output_len, const char *input) {
+    return euicc_hexutil_hex2bin_r(output, output_len, input, strlen(input));
+}
+
 int euicc_hexutil_hex2bin_r(uint8_t *output, const uint32_t output_len, const char *input, const uint32_t input_len) {
     if (output == NULL || input == NULL || (input_len % 2) != 0 || output_len < (input_len / 2)) {
         return -1;
@@ -45,13 +62,11 @@ int euicc_hexutil_bin2hex(char *output, const uint32_t output_len, const uint8_t
     if (output == NULL || bin == NULL || output_len < ((2 * bin_len) + 1)) {
         return -1;
     }
-    uint8_t msb, lsb;
+    static const char digits[] = "0123456789abcdef";
     uint32_t bytes = 0;
-    for (uint32_t i = 0; i < bin_len; ++i) {
-        msb = bin[i] >> 4;
-        lsb = bin[i] & 0xf;
-        output[bytes++] = (char)(msb + (msb > 9 ? 'a' - 10 : '0'));
-        output[bytes++] = (char)(lsb + (lsb > 9 ? 'a' - 10 : '0'));
+    for (uint32_t i = 0; i < bin_len; i++) {
+        output[bytes++] = digits[bin[i] >> 4];
+        output[bytes++] = digits[bin[i] & 0xf];
     }
     output[bytes] = '\0';
     return 0;
@@ -68,39 +83,26 @@ int euicc_hexutil_gsmbcd2bin(uint8_t *output, const uint32_t output_len, const c
     }
     memcpy(bin, input, n);
     memset(bin + n, 'f', n % 2); // pad with 'f' if odd
+    gsmbcd_swap_chars(bin, n + (n % 2));
     const int bytes = euicc_hexutil_hex2bin_r(output, output_len, bin, n + (n % 2));
     free(bin);
     if (bytes == -1) {
         return -1;
-    }
-    for (int i = 0; i < bytes; i++) {
-        output[i] = output[i] >> 4 | (output[i] & 0xf) << 4; // swap
     }
     memset(output + bytes, 0xff, padding_to - bytes);
     return bytes;
 }
 
 int euicc_hexutil_bin2gsmbcd(char *output, const uint32_t output_len, const uint8_t *bin, const uint32_t bin_len) {
-    if (output == NULL || bin == NULL || output_len < ((2 * bin_len) + 1)) {
+    if (euicc_hexutil_bin2hex(output, output_len, bin, bin_len) < 0) {
         return -1;
     }
-    uint32_t i;
-    uint8_t *tmp = calloc(bin_len, sizeof(uint8_t));
-    if (tmp == NULL) {
-        return -1;
-    }
-    for (i = 0; i < bin_len; i++) {
-        tmp[i] = bin[i] >> 4 | (bin[i] & 0xf) << 4; // swap
-    }
-    const int ret = euicc_hexutil_bin2hex(output, output_len, tmp, bin_len);
-    free(tmp);
-    if (ret != 0) {
-        return -1;
-    }
-    i = (2 * bin_len) - 1;
+    gsmbcd_swap_chars(output, output_len);
+    // trim trailing 'f'
+    uint32_t i = (2 * bin_len) - 1;
     while (i > 0 && output[i] != 'f') {
         i--;
     }
-    output[i] = 0;
+    output[i] = '\0';
     return 0;
 }
